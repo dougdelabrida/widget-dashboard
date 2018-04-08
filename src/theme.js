722a48d530862dd47d83f87f294f70df55ec4a55
Douglas Delabrida <dougdelabrida@gmail.com>
2018-04-08 05:07:23 -0300

    changed blackGroundOpacity

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,7 @@
 const theme = {
   blueBackground: 'linear-gradient(to left, #3a7bd5, #3a6073)',
   defaultFont: 'Montserrat, sans-serif',
-  blackBackgroundOpacity: 'rgba(0, 0, 0, .15)'
+  blackBackgroundOpacity: 'rgba(5, 33, 37, 0.95)'
 };
 
 export default theme;
